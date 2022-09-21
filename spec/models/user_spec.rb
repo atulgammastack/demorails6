5c55db25b57f5f0ac5da3df4efc0aff58b13b226
atul patidar <apatidar@grepruby.io>
2022-09-21 13:57:38 +0530

    Test model level test was passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,19 +20,19 @@ RSpec.describe User, type: :model do
     end
     it "is not valid without a email " do
       subject.email = nil
-      expect(subject).to be_valid
+      expect(subject).to_not be_valid
     end
     it "is not valid without a password " do
       subject.password = nil
-      expect(subject).to be_valid
+      expect(subject).to_not be_valid
     end
     it "is not valid with less than 3 charcters first_name)  " do
       subject.first_name = "at"
-      expect(subject).to be_valid
+      expect(subject).to_not be_valid
     end
     it 'ensure save succesfully presence' do
       user = User.create(first_name: 'first', username: 'username22', email: 'aptidar@grescsdddby.rsa', password: '123456')
-      expect(user).to be_valid
+      expect(user).to_not be_valid
     end
   end
 end
